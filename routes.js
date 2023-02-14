d7e578b62e80fad017959a2859e527ebffce040a
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-02-14 11:38:10 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,9 +1,7 @@
-// This file was automatically added by edgio deploy.
+// This file was automatically added by edgio init.
 // You should commit this file to source control.
+
 import { Router } from '@edgio/core'
 import { hexoRoutes } from '@edgio/hexo'
 
-export default new Router()
-  // Prevent search engines from indexing permalink URLs
-  .noIndexPermalink()
-  .use(hexoRoutes)
+export default new Router().use(hexoRoutes)
